a07834e16f24585225388b6eec2385106f0320cd
ali_aminiام شما <aliamini13821384@gmail.com >
2025-09-21 21:02:22 +0330

    Updates

--- assets/DictionaryPage-DizgUiFd.js
+++
@@ -1,2 +0,0 @@
-import{j as t,N as i}from"./index-Dhx_KrQB.js";import"./vendor-DoC2WAmd.js";function a(){return t.jsx(i,{})}export{a as default};
-//# sourceMappingURL=DictionaryPage-DizgUiFd.js.map
---
+++ assets/UserAccountPage-DZKrqKLR.js
@@ -0,0 +1,2 @@
+import{j as r}from"./index-CKiFFH6U.js";import"./vendor-DoC2WAmd.js";function o(){return r.jsx("div",{children:"UserAccountPage"})}export{o as default};
+//# sourceMappingURL=UserAccountPage-DZKrqKLR.js.map
